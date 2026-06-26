85275bf3a7de2f85751efd1d9c81abb642b29a77
Kashika Ghosh <kashika0613@gmail.com>
2026-06-26 18:48:14 +0530

    user index setup

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const userModel = require("./models/user")
+const cookieParser = require("cookie-parser");
+
+app.set("view engine", "ejs");
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 
 app.get("/",(req,res)=> {
-    res.send("helllo");
+    res.render("index");
 })
 app.listen(3000,()=> {
     console.log("server is working on port 3000");
